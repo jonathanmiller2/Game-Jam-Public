5d193a6bedc0d6e27a1b9679bf845590a30a2162
Alexzoin <alex.cecrle@gmail.com>
2019-05-31 00:23:37 -0500

    Implemented sounds and volume slider.

--- Assets/Scripts/Audio/ChangeSongs.cs
+++ Assets/Scripts/Audio/ChangeSongs.cs
@@ -6,6 +6,7 @@ public class ChangeSongs : MonoBehaviour
 {
 	public string[] playSongNames = new string[10];
 	public string[] fadeOutSongNames = new string[10];
+	public string[] fadeInSongNames = new string[10];
 
 	void Start()
     {
@@ -22,7 +23,13 @@ public class ChangeSongs : MonoBehaviour
 		{
 			FindObjectOfType<AudioManager>().Play(songName);
 		}
-		
+
+		//fade in songs to fade
+		foreach (string songName in fadeInSongNames)
+		{
+			FindObjectOfType<AudioManager>().FadeIn(songName);
+		}
+
 	}
 
 }
