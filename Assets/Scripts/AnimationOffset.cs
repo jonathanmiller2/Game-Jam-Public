2d8b241bf5bcc55d9caf51f12f40b8c7e26d1242
Camzoin <Camden.Cecrle@gmail.com>
2019-06-01 14:15:24 -0700

    Updated main menu and a lot of other art stuff

--- Assets/Scripts/AnimationOffset.cs
+++ Assets/Scripts/AnimationOffset.cs
@@ -6,7 +6,8 @@ public class AnimationOffset : MonoBehaviour
 {
     private void Start()
     {
-         Animator anim = GetComponent<Animator>();
+        gameObject.transform.rotation = Quaternion.Euler(0f, 0f, Random.Range(0f, 360f));
+        Animator anim = GetComponent<Animator>();
          AnimatorStateInfo state = anim.GetCurrentAnimatorStateInfo(0);//could replace 0 by any other animation layer index
          anim.Play(state.fullPathHash, -1, Random.Range(0f, 1f));
     }
