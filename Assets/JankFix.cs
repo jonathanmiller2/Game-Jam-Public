a4ec7b5a9370e65a72c8a45a313310d46cc20915
Camzoin <Camden.Cecrle@gmail.com>
2019-06-01 23:34:45 -0700

    bad

--- Assets/JankFix.cs
+++ Assets/JankFix.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class JankFix : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 0, 0);
+        transform.Rotate(1, 1, 1);
     }
 }
