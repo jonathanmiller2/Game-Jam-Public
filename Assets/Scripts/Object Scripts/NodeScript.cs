77e1de8cccd81e9a5f81fafc5aeb5403ab575037
jonathanmiller2 <jonathanmiller2@hotmail.com>
2019-06-01 23:39:56 -0500

    Updated Node Script

--- Assets/Scripts/Object Scripts/NodeScript.cs
+++ Assets/Scripts/Object Scripts/NodeScript.cs
@@ -9,6 +9,9 @@ public class NodeScript : MonoBehaviour, IPointerClickHandler
     [Range(0.001f, 1f)]
     public float Radius = .13f;
     public int Owner = 0;
+
+    //Colors
+    public Material[] myMaterials;
     
     public GameObject GhostBridgePiecePrefab;
     public GameObject AttackerPrefab;
@@ -19,6 +22,15 @@ public class NodeScript : MonoBehaviour, IPointerClickHandler
     //Conversion data
     private float CheckTimer = 0f;
     private float SecondsPerCheck = 1f;
+
+    private float ConversionTimer = 0f;
+    private float SecondsForConversion = 0f;
+
+    private float ValidAttackerRadius = .6f;
+    private List<int> AttackerCountsOnNode;
+    private int NextOwner;
+
+    
 
     // Start is called before the first frame update
     void Start()
@@ -36,21 +48,95 @@ public class NodeScript : MonoBehaviour, IPointerClickHandler
 
         //TODO: 
 
+        float Dist = 1000f;
+        bool tie = true;
+
         //Check once a second how many attackers we have on our node
         //If it's imbalanced start a conversion timer
-
         if(CheckTimer > SecondsPerCheck)
         {
         	CheckTimer = 0;
         	
         	//Check how many attackers are on this node
+        	foreach (GameObject obj in GameObject.FindGameObjectsWithTag("Attacker"))
+        	{
+        	    Dist = Vector3.Distance(GetChildObjectWithTag(obj.transform, "CenterPoint").transform.position, gameObject.transform.position);
+	
+        	    if (Dist < ValidAttackerRadius)
+        	    {
+        	        int OwnerOfAttacker = obj.GetComponent<AttackerScript>().GetOwner();
+        	        if(AttackerCountsOnNode[OwnerOfAttacker] == null)
+        	        {
+        	        	AttackerCountsOnNode[OwnerOfAttacker] = 1;
+        	        }
+        	        else
+        	        {
+        	        	AttackerCountsOnNode[OwnerOfAttacker] += 1;
+        	        }
+        	    }
+        	}
 
-
+        	//Find owner with highest attacker count
+        	int HighestAmountOfAttackers = -1;
+        	int OwnerOfHighestAttackers = 0;
+        	for(int i = 0; i < AttackerCountsOnNode.Count; i++)
+        	{
+        		if(AttackerCountsOnNode[i] > HighestAmountOfAttackers)
+        		{
+        			HighestAmountOfAttackers = AttackerCountsOnNode[i];
+        			OwnerOfHighestAttackers = i;
+        		}
+        	}
+	
+	
+        	//Check if there's a tie
+        	for(int i = 0; i < AttackerCountsOnNode.Count; i++)
+        	{
+        		//Don't check a tie with ourself
+        		if(i != OwnerOfHighestAttackers)
+        		{
+        			if(AttackerCountsOnNode[i] == HighestAmountOfAttackers)
+        			{
+        				tie = true;
+        				break;
+        			}
+        		}
+        	}
+	
+        	if(!tie)
+       	 	{
+        		NextOwner = OwnerOfHighestAttackers;
+        	}
         }
         else
         {
         	CheckTimer += Time.deltaTime;
-        }        
+        }
+
+        //Check if we have an imbalance of power
+        if(tie)
+        {
+        	NextOwner = Owner;
+        	ConversionTimer = 0;
+        }
+        else
+        {
+        	if(ConversionTimer > SecondsForConversion)
+        	{
+        		ConversionTimer = 0;
+
+        		//TODO: Conversion!
+        		//Animation? Set material?
+        		Owner = NextOwner;
+        	}
+        	else
+        	{
+        		ConversionTimer += Time.deltaTime;
+        	}
+        }
+
+       
+
     }
 
     public void OnPointerClick(PointerEventData InputPointerEventData)
@@ -81,6 +167,25 @@ public class NodeScript : MonoBehaviour, IPointerClickHandler
         	//We clicked on this node and we don't own it
         	//TODO: attack
         }
+    }
+
+    //(single)
+    public GameObject GetChildObjectWithTag(Transform Parent, string Tag)
+    {
+        for (int i = 0; i < Parent.childCount; i++)
+        {
+            Transform Child = Parent.GetChild(i);
+            if (Child.tag == Tag)
+            {
+                return Child.gameObject;
+            }
+            if (Child.childCount > 0)
+            {
+                GetChildObjectWithTag(Child, Tag);
+            }
+        }
+
+        return null;
     }
 
     public float GetRadius()
