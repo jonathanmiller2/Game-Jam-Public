33bcd66e126a052af93633d0d1e9e68cb058b6ab
Camzoin <Camden.Cecrle@gmail.com>
2019-06-02 13:24:04 -0700

    Touched most prefabs, added play animation

--- Assets/Scripts/AnimationOffset.cs
+++ Assets/Scripts/AnimationOffset.cs
@@ -9,7 +9,7 @@ public class AnimationOffset : MonoBehaviour
         gameObject.transform.rotation = Quaternion.Euler(0f, 0f, Random.Range(0f, 360f));
         Animator anim = GetComponent<Animator>();
          AnimatorStateInfo state = anim.GetCurrentAnimatorStateInfo(0);//could replace 0 by any other animation layer index
-         anim.Play(state.fullPathHash, -1, Random.Range(0f, 1f));
+         anim.Play(state.fullPathHash, -1, Random.Range(0.5f, 0.8f));
     }
 }
    /* void Update()
--- Assets/Scripts/Menus/MainMenuScript.cs
+++ Assets/Scripts/Menus/MainMenuScript.cs
@@ -11,7 +11,7 @@ public class MainMenuScript : MonoBehaviour
     public void PlayButtonPressed()
     {
 		FindObjectOfType<AudioManager>().Play("Menu Play");
-		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+		//SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void ExitButtonPressed()
