3a818ca16e4b68a4f4e4bed3b9e9258965c84a02
Alexzoin <alex.cecrle@gmail.com>
2019-06-02 23:47:22 -0500

    Made HUD work.

---
+++ Assets/HUDController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class HUDController : MonoBehaviour
+{
+
+	private InputControllerScript inputController;
+
+    void Start()
+    {
+		inputController = FindObjectOfType<InputControllerScript>();
+
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		foreach (TextMeshProUGUI mesh in transform.GetComponentsInChildren<TextMeshProUGUI>())
+		{
+			if (mesh.tag == "PPS")
+			{
+				mesh.text = "" + inputController.GetPointsPerTime();
+			}
+			else if (mesh.tag == "Points")
+			{
+				mesh.text = "" + inputController.Points;
+			}
+			
+		}
+    }
+}
