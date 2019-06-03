51f8211879d75d453be7a3375c843945bfbd94b1
Camzoin <Camden.Cecrle@gmail.com>
2019-06-02 22:40:09 -0700

    Made hud smaller

--- Assets/Scripts/Object Scripts/CursorController.cs
+++ Assets/Scripts/Object Scripts/CursorController.cs
@@ -7,16 +7,26 @@ public class CursorController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-		DontDestroyOnLoad(gameObject);
-		
+        DontDestroyOnLoad(gameObject);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-		Cursor.visible = false;
-		gameObject.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-		gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, 1f);
-		// Debug.Log(gameObject.transform.position);
-	}
+        Cursor.visible = false;
+        gameObject.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, 1f);
+        // Debug.Log(gameObject.transform.position);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+
+        }
+    }
 }
