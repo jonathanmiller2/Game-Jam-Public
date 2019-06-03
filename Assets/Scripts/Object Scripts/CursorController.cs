40df3a5e4f7ecd434782a6a713d3cfa12c7f4fcc
Camzoin <Camden.Cecrle@gmail.com>
2019-06-02 23:26:54 -0700

    cursor updoot

--- Assets/Scripts/Object Scripts/CursorController.cs
+++ Assets/Scripts/Object Scripts/CursorController.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class CursorController : MonoBehaviour
 {
     public Material[] material;
-    public GameObject cursor;
 
     // Start is called before the first frame update
     void Start()
